1b478fdee8388d282b1f404722e27e0c44fe4738
GordeyGudim <gordey.gudim@mail.ru>
2022-04-06 18:31:22 +0300

    Komunism

--- python.py
+++ python.py
@@ -1,9 +1,3 @@
-import random
-def orel_reshka(vibros = random.choice(range(1,3))):
-    n = int(input())
-    if n == vibros:
-        return 'you win'
-    else:
-        print('You not win')
-        return orel_reshka()
-print(orel_reshka())
+name = input('Are You a Comunist?y/n')
+if name == 'y':
+    print('krasava bro')
